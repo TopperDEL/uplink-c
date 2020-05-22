41cd68d7b48da5ddca30be614ac598b48ccb0a5e
TopperDEL <github_com@tparth.de>
2020-05-22 14:41:03 +0200

    Update build-macos-ios.sh

--- build-macos-ios.sh
+++ build-macos-ios.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "*** Build for MacOS ***"
-go build -buildmode c-shared -o storj_uplink-macos.dylib
+go build -buildmode c-shared -o "build/macOS/storj_uplink-macos.dylib"
 
 echo "*** Build for iOs x86 ***"
 env \
@@ -13,7 +13,7 @@ GOARCH=arm \
 GOARM=7 \
 CGO_CFLAGS="-arch armv7 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.4.sdk -miphoneos-version-min=10.0" \
 CGO_LDFLAGS="-arch armv7 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.4.sdk -miphoneos-version-min=10.0" \
-go build -ldflags -w -v -installsuffix goi -o "arm/storj_uplink.a" -buildmode=c-archive
+go build -ldflags -w -v -tags ios -o "build/arm/storj_uplink.a" -buildmode=c-archive
 
 echo "*** Build for iOs x64 ***"
 env \
@@ -24,7 +24,7 @@ CGO_ENABLED=1 \
 GOARCH=arm64 \
 CGO_CFLAGS="-arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.4.sdk -miphoneos-version-min=12.0" \
 CGO_LDFLAGS="-arch arm64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS13.4.sdk -miphoneos-version-min=12.0" \
-go build -ldflags -w -v -installsuffix goi -o "arm64/storj_uplink.a" -buildmode=c-archive
+go build -ldflags -w -v -tags ios -o "build/arm64/storj_uplink.a" -buildmode=c-archive
 
 echo "*** Build for iOs-Simulator x86 ***"
 env \
@@ -35,8 +35,7 @@ CGO_ENABLED=1 \
 GOARCH=386 \
 CGO_CFLAGS="-arch i386 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.4.sdk" \
 CGO_LDFLAGS="-arch i386 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.4.sdk" \
-go build -ldflags -w -v -installsuffix goi -o "sim86/storj_uplink.a" -buildmode=c-archive
-
+go build -ldflags -w -v -tags ios -o "build/sim86/storj_uplink.a" -buildmode=c-archive
 
 echo "*** Build for iOs-Simulator x64 ***"
 env \
@@ -47,4 +46,4 @@ CGO_ENABLED=1 \
 GOARCH=amd64 \
 CGO_CFLAGS="-arch x86_64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.4.sdk" \
 CGO_LDFLAGS="-arch x86_64 -isysroot /Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator13.4.sdk" \
-go build -ldflags -w -v -installsuffix goi -o "amd64/storj_uplink.a" -buildmode=c-archive
+go build -ldflags -w -v -tags ios -o "build/sim64/storj_uplink.a" -buildmode=c-archive
